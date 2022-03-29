90b2b1b349dde1fa6f204dc73e9542439659c754
Nayana Loivos <loivosdev@gmail.com>
2022-03-29 11:03:53 -0300

    Configurando a template engine handlebars e a conexão com o MySQL

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html');
-})
+//Config
+    //Template Engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+    //conexão com MySQL
+    const Sequelize = require('sequelize');
+    const sequelize = new Sequelize('nomedobancodedados', 'root', '123456', {
+    host: 'localhost',
+    dialect: 'mysql' 
+});
+
+
+
 
 app.listen(8081, () => {
     console.log('SERVIDOR RODANDO NA URL http://localhost:8081');
